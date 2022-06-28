917ae2f1949f9472136b4aaf0d943fe501792af6
Matt N Tran <nnguye44@jhmi.edu>
2022-06-28 00:39:25 -0400

    re-compute markers and print lists with correct merges

--- snRNAseq_mouse/code/02_analyses/04_markerDetection.R
+++ snRNAseq_mouse/code/02_analyses/04_markerDetection.R
@@ -41,26 +41,7 @@ load(here("snRNAseq_mouse", "processed_data","SCE", "sce_updated_LS.rda"), verbo
     # sce.ls, annotationTab.ls, cell_colors.ls
 
 table(sce.ls$cellType.final)
-    #  Aqp4.Rbpms              Astro       drop.doublet drop.likelyDoublet 
-    #         486               5682                150                573 
-    # drop.lowNTx               Endo          Ependymal            Excit_A 
-    #         180                158                 65                485 
-    #     Excit_B            Excit_C            Excit_D            Excit_E 
-    #         670                232                 67                 31 
-    #     Excit_F            Excit_G            Inhib_A            Inhib_B 
-    #         202                728                797                760 
-    #     Inhib_C            Inhib_D            Inhib_E            Inhib_F 
-    #         191               1075                306               4727 
-    #     Inhib_G            Inhib_H            Inhib_I            Inhib_J 
-    #        1524                 86                111                402 
-    #     Inhib_K            Inhib_L            Inhib_M            Inhib_N 
-    #          36                167                 45                 69 
-    #     Inhib_O            Inhib_P            Inhib_Q              Micro 
-    #         190                 36                 68                166 
-    #       Mural       Neuron.mixed              Oligo                OPC 
-    #         222                 73               1583                464 
-    #     OPC_COP 
-    #          53 
+    # 
 
 ## doubletScore distributions / cluster?
 cellClust.idx <- splitit(sce.ls$cellType.final)
@@ -71,24 +52,29 @@ sapply(cellClust.idx, function(x){round(quantile(sce.ls$doubletScore[x]), 2)})
     # 50%        0.10  0.18         4.83               2.01        0.03 0.06
     # 75%        0.46  0.48         6.72               2.94        0.06 0.17
     # 100%      10.39 14.84         9.39              11.38        7.93 5.98
-    #      Ependymal Excit_A Excit_B Excit_C Excit_D Excit_E Excit_F Excit_G Inhib_A
-    # 0%        0.02    0.00    0.02    0.03    0.04    0.12    0.02    0.04    0.01
-    # 25%       0.36    0.04    0.15    0.14    0.38    0.61    0.13    0.17    0.13
-    # 50%       0.49    0.07    0.39    0.46    1.28    0.78    0.23    0.30    0.25
-    # 75%       0.53    0.27    1.31    1.30    1.39    1.21    0.52    1.03    0.62
-    # 100%      7.10   12.53   10.41    6.82    9.93    7.56    7.62    9.25   10.44
-    #      Inhib_B Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G Inhib_H Inhib_I Inhib_J
-    # 0%      0.00    0.01    0.01    0.00    0.00    0.00    0.09    0.00    0.00
-    # 25%     0.03    0.23    0.17    0.02    0.15    0.13    0.30    0.12    0.13
-    # 50%     0.07    0.43    0.36    0.06    0.29    0.27    0.52    0.23    0.28
-    # 75%     0.78    1.08    0.93    0.52    0.90    0.74    0.91    0.87    0.67
-    # 100%   11.46    5.98   11.49    6.60   10.09   11.42    8.40    5.92   11.43
-    #      Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P Inhib_Q Micro Mural
+
+    #      Ependymal Excit_A Excit_B Excit_C Excit_D Excit_E Excit_F Inhib_A Inhib_B
+    # 0%        0.02    0.00    0.02    0.03    0.04    0.12    0.02    0.01    0.00
+    # 25%       0.36    0.04    0.15    0.14    0.38    0.61    0.16    0.13    0.03
+    # 50%       0.49    0.07    0.39    0.46    1.28    0.78    0.29    0.25    0.07
+    # 75%       0.53    0.27    1.31    1.30    1.39    1.21    0.85    0.62    0.78
+    # 100%      7.10   12.53   10.41    6.82    9.93    7.56    9.25   10.44   11.46
+
+
+    #      Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G Inhib_H Inhib_I Inhib_J Inhib_K
+    # 0%      0.01    0.01    0.00    0.00    0.00    0.09    0.00    0.00    0.00
+    # 25%     0.23    0.17    0.02    0.16    0.04    0.30    0.13    0.12    0.13
+    # 50%     0.43    0.36    0.06    0.29    0.11    0.52    0.27    0.23    0.28
+    # 75%     1.08    0.93    0.52    0.90    0.84    0.91    0.74    0.87    0.67
+    # 100%    5.98   11.49    6.60   10.09    9.50    8.40   11.42    5.92   11.43
+
+    #      Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P Inhib_Q Inhib_R Micro Mural
     # 0%      0.01    0.01    0.06    0.13    0.02    0.04    0.07  0.00  0.00
     # 25%     0.26    0.11    0.32    0.28    0.17    0.19    0.16  0.06  0.04
     # 50%     0.39    0.18    0.42    0.40    0.25    0.34    0.23  0.09  0.05
     # 75%     0.94    0.30    0.74    0.79    0.42    0.84    0.34  0.17  0.07
     # 100%    9.41    7.01    2.50    8.65    7.82    6.07    3.02  3.62  9.43
+
     #      Neuron.mixed Oligo  OPC OPC_COP
     # 0%           0.04  0.00 0.00    0.05
     # 25%          0.17  0.03 0.01    0.24
@@ -104,28 +90,28 @@ sapply(cellClust.idx, function(x){quantile(sce.ls$sum[x])})
     # 75%       11051  4991.75      8601.25              12432      1872.5  9237.75
     # 100%     181764 32378.00     19299.00              56422     29767.0 48895.00
 
-    #      Ependymal Excit_A Excit_B  Excit_C Excit_D Excit_E  Excit_F  Excit_G
-    # 0%        3270     980  1377.0  3157.00  3191.0    5919  3194.00  1972.00
-    # 25%       5620    4952  8945.0  9379.25  5481.5    9051 10119.75 10323.25
-    # 50%       7739    6694 12002.5 13230.50  8429.0   10714 14389.50 14305.00
-    # 75%      12165    9044 16862.0 18958.50 10386.5   14075 19935.75 20099.00
-    # 100%     38139   43894 64847.0 58550.00 23709.0   35325 60231.00 54045.00
+    #      Ependymal Excit_A Excit_B  Excit_C Excit_D Excit_E Excit_F Inhib_A
+    # 0%        3270     980  1377.0  3157.00  3191.0    5919  1972.0    1947
+    # 25%       5620    4952  8945.0  9379.25  5481.5    9051 10227.0    5937
+    # 50%       7739    6694 12002.5 13230.50  8429.0   10714 14337.0    8099
+    # 75%      12165    9044 16862.0 18958.50 10386.5   14075 20052.5   11043
+    # 100%     38139   43894 64847.0 58550.00 23709.0   35325 60231.0   53149
 
-    #      Inhib_A  Inhib_B Inhib_C Inhib_D  Inhib_E  Inhib_F  Inhib_G  Inhib_H
-    # 0%      1947   933.00  3595.0  1069.0  1707.00    850.0  1768.00  2817.00
-    # 25%     5937  2680.00  9057.5  6909.5  3605.50   7688.5  6224.00  7266.25
-    # 50%     8099  4112.00 12421.0  9056.0  5034.00  10395.0  8654.00  9130.50
-    # 75%    11043  7178.75 17005.0 12044.5  7344.25  13950.0 11837.75 13190.75
-    # 100%   53149 83174.00 33512.0 45681.0 40784.00 134792.0 51958.00 50031.00
+    #       Inhib_B Inhib_C Inhib_D  Inhib_E   Inhib_F Inhib_G  Inhib_H  Inhib_I
+    # 0%     933.00  3595.0  1069.0  1707.00    850.00  2053.0  2817.00  1768.00
+    # 25%   2680.00  9057.5  6909.5  3605.50   7810.75  6602.0  7266.25  6224.00
+    # 50%   4112.00 12421.0  9056.0  5034.00  10514.50  8630.0  9130.50  8654.00
+    # 75%   7178.75 17005.0 12044.5  7344.25  13983.25 13227.5 13190.75 11837.75
+    # 100% 83174.00 33512.0 45681.0 40784.00 134792.00 47249.0 50031.00 51958.00
 
-    #      Inhib_I  Inhib_J  Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O  Inhib_P
+    #      Inhib_J  Inhib_K  Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P  Inhib_Q
     # 0%    5982.0  2179.00  4684.00    3068    4600    3005  6673.0  5602.00
     # 25%  11439.5  7938.00  8697.00    7046    8307    6080 11536.5  8687.25
     # 50%  16729.0 10361.00 11063.50    9079   11863    7613 13859.5 10348.50
     # 75%  21484.5 13901.75 12831.25   11844   16078   10506 17912.5 13666.25
     # 100% 57135.0 44825.00 36869.00   36314   40109   21836 57038.0 26839.00
 
-    #       Inhib_Q    Micro    Mural Neuron.mixed   Oligo      OPC OPC_COP
+    #       Inhib_R    Micro    Mural Neuron.mixed   Oligo      OPC OPC_COP
     # 0%    5720.00   881.00   737.00         3855   847.0  1037.00    2805
     # 25%  11226.75  2009.25  2244.75         8877  2216.5  3564.25    5800
     # 50%  13534.50  2908.50  3384.00        12019  2947.0  5289.00    7404
@@ -168,35 +154,22 @@ medianNon0.ls <- lapply(cellClust.idx, function(x){
 })
 
 sapply(medianNon0.ls, table)
-    #      Aqp4.Rbpms Astro  Endo Ependymal Excit_A Excit_B Excit_C Excit_D Excit_E
+    #       Aqp4.Rbpms Astro  Endo Ependymal Excit_A Excit_B Excit_C Excit_D Excit_E
     # FALSE      25636 27052 26439     25184   26193   24722   24492   25803   24898
     # TRUE        2115   699  1312      2567    1558    3029    3259    1948    2853
 
-    #       Excit_F Excit_G Inhib_A Inhib_B Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G
-    # FALSE   24414   24422   25734   26804   24630   25312   26549   25221   25505
-    # TRUE     3337    3329    2017     947    3121    2439    1202    2530    2246
+    #       Excit_F Inhib_A Inhib_B Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G Inhib_H
+    # FALSE   24428   25734   26804   24630   25312   26549   25217   25480   25242
+    # TRUE     3323    2017     947    3121    2439    1202    2534    2271    2509
 
-    #       Inhib_H Inhib_I Inhib_J Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P
-    # FALSE   25242   23913   25004   24636   25474   24957   25698   24134   24702
-    # TRUE     2509    3838    2747    3115    2277    2794    2053    3617    3049
+    #       Inhib_I Inhib_J Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P Inhib_Q
+    # FALSE   25505   23913   25004   24636   25474   24957   25698   24134   24702
+    # TRUE     2246    3838    2747    3115    2277    2794    2053    3617    3049
 
-    #       Inhib_Q Micro Mural Oligo   OPC OPC_COP
+    #       Inhib_R Micro Mural Oligo   OPC OPC_COP
     # FALSE   24050 27078 27209 27217 26439   25887
     # TRUE     3701   673   542   534  1312    1864
 
-
-# # Just for interactive exploration of some of these
-# plotExpressionCustom(sce = sce.ls,
-#                      exprs_values = "logcounts",
-#                      #
-#                      features = head(Oligo_A_markers,4),
-#                      features_name = "custom-selected",
-#                      anno_name = "cellType.final",
-#                      ncol=2, point_alpha=0.4, point_size=0.9,
-#                      scales="free_y", swap_rownames="gene_name") +
-#     ggtitle(label=paste0("Lionel's custom markers of interest")) +
-#     theme(plot.title = element_text(size = 12),
-#           axis.text.x = element_text(size=7))
 
 
 ## Traditional t-test, pairwise ===
@@ -224,19 +197,20 @@ for(i in names(markers.ls.t.pw)){
 
 sapply(markers.ls.t.pw, function(x){table(x$FDR<0.05 & x$non0median == TRUE)["TRUE"]})
     #Aqp4.Rbpms.TRUE      Astro.TRUE       Endo.TRUE  Ependymal.TRUE    Excit_A.TRUE 
-    #            496              98             328             272              47 
+    #            493              98             328             270              46 
     #   Excit_B.TRUE    Excit_C.TRUE    Excit_D.TRUE    Excit_E.TRUE    Excit_F.TRUE 
-    #             26              15              69              24              24 
-    #   Excit_G.TRUE    Inhib_A.TRUE    Inhib_B.TRUE    Inhib_C.TRUE      Inhib_D.NA 
-    #              2               3              34              14              NA 
-    #   Inhib_E.TRUE    Inhib_F.TRUE      Inhib_G.NA    Inhib_H.TRUE    Inhib_I.TRUE 
-    #             41              13              NA              36               6 
-    #   Inhib_J.TRUE    Inhib_K.TRUE    Inhib_L.TRUE    Inhib_M.TRUE    Inhib_N.TRUE 
-    #              1              25              36              14              15 
-    #   Inhib_O.TRUE    Inhib_P.TRUE    Inhib_Q.TRUE      Micro.TRUE      Mural.TRUE 
-    #              2               5              21             180              17 
+    #             25              16              69              25              14 
+    #   Inhib_A.TRUE    Inhib_B.TRUE    Inhib_C.TRUE      Inhib_D.NA    Inhib_E.TRUE 
+    #              2              29              14              NA              43 
+    #   Inhib_F.TRUE    Inhib_G.TRUE    Inhib_H.TRUE      Inhib_I.NA    Inhib_J.TRUE 
+    #             26              41              31              NA               6 
+    #   Inhib_K.TRUE    Inhib_L.TRUE    Inhib_M.TRUE    Inhib_N.TRUE    Inhib_O.TRUE 
+    #              1              24              36              14              17 
+    #   Inhib_P.TRUE    Inhib_Q.TRUE    Inhib_R.TRUE      Micro.TRUE      Mural.TRUE 
+    #              2               4              20             180              17 
     #     Oligo.TRUE        OPC.TRUE    OPC_COP.TRUE 
-    #            119              50             141 
+    #            118              50             141 
+
 
 ## Save these
 save(markers.ls.t.pw, medianNon0.ls,
@@ -379,15 +353,15 @@ sapply(markers.ls.t.1vAll, function(x){
     # FALSE      26399 27270 26938     26714   27036   26120   26740   27243   27395
     # TRUE        1352   481   813      1037     715    1631    1011     508     356
 
-    #       Excit_F Excit_G Inhib_A Inhib_B Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G
-    # FALSE   26935   26309   26845   27193   26882   26237   27222   26237   26350
-    # TRUE      816    1442     906     558     869    1514     529    1514    1401
+    #       Excit_F Inhib_A Inhib_B Inhib_C Inhib_D Inhib_E Inhib_F Inhib_G Inhib_H
+    # FALSE   26282   26845   27193   26882   26237   27222   26227   27109   27248
+    # TRUE     1469     906     558     869    1514     529    1524     642     503
 
-    #       Inhib_H Inhib_I Inhib_J Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P
-    # FALSE   27248   27028   26528   27446   26903   27451   27328   26648   27452
-    # TRUE      503     723    1223     305     848     300     423    1103     299
+    #       Inhib_I Inhib_J Inhib_K Inhib_L Inhib_M Inhib_N Inhib_O Inhib_P Inhib_Q
+    # FALSE   26350   27028   26528   27446   26903   27451   27328   26648   27452
+    # TRUE     1401     723    1223     305     848     300     423    1103     299
 
-    #       Inhib_Q Micro Mural Oligo   OPC OPC_COP
+    #       Inhib_R Micro Mural Oligo   OPC OPC_COP
     # FALSE   26921 27338 27529 27367 27213   27300
     # TRUE      830   413   222   384   538     451
 
@@ -448,14 +422,13 @@ write.csv(top40genes, file=here("snRNAseq_mouse", "processed_data","tables",
 ## Reproducibility information ====
 print('Reproducibility information:')
 Sys.time()
-#[1] "2022-06-24 16:42:42 EDT"
-
+#[1] "2022-06-27 18:59:20 EDT"
 proc.time()
 #     user    system   elapsed 
-#11692.065   150.947 11849.534
+#11772.950   155.072 11964.713 
 options(width = 120)
 session_info()
-#─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
 # setting  value
 # version  R version 4.1.2 Patched (2021-11-04 r81138)
 # os       CentOS Linux 7 (Core)
@@ -465,7 +438,7 @@ session_info()
 # collate  en_US.UTF-8
 # ctype    en_US.UTF-8
 # tz       US/Eastern
-# date     2022-06-24
+# date     2022-06-27
 # pandoc   2.13 @ /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/bin/pandoc
 # 
 # ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
