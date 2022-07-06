f08bc95ebc25387bdda6a19ff8f5e13e109e8ae9
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-07-06 18:08:47 -0400

    Version contorlling the changes in deploy.R script

--- snRNAseq_mouse/code/03_iSEE_apps/snRNA-seq_app/deploy.R
+++ snRNAseq_mouse/code/03_iSEE_apps/snRNA-seq_app/deploy.R
@@ -4,8 +4,10 @@ library('rsconnect')
 source("token.R")
 
 options(repos = BiocManager::repositories())
-rsconnect::deployApp(appFiles = c('app.R', "sce_for_iSEE_LS.rda", "initial.R"),
+rsconnect::deployApp(
+    appFiles = c('app.R', "sce_for_iSEE_LS.rda", "initial.R"),
     appName = 'LS_mm_2022',
     account = 'libd',
-    server = 'shinyapps.io')
+    server = 'shinyapps.io'
+)
 
