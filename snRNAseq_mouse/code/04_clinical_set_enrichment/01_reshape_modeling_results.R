f615ff66b3da791a54618e09bc8a2ead7951ac20
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-03-02 16:02:51 -0600

    Code documentation

--- snRNAseq_mouse/code/04_clinical_set_enrichment/01_reshape_modeling_results.R
+++ snRNAseq_mouse/code/04_clinical_set_enrichment/01_reshape_modeling_results.R
@@ -1,3 +1,4 @@
+## Call libraries
 library("here")
 library("sessioninfo")
 library("biomaRt")
@@ -25,16 +26,32 @@ lobstr::obj_size(markers.ls.t.pw.broad)
 lobstr::obj_size(markers.ls.t.1vAll.broad)
 # 63.08 MB
 
-# class(markers.ls.t.1vAll.broad)
-# names(markers.ls.t.1vAll.broad)
-# class(markers.ls.t.1vAll.broad$LS)
-# names(markers.ls.t.1vAll.broad$LS)
-# class(markers.ls.t.1vAll.broad$LS$LS_enriched)
+class(markers.ls.t.1vAll.broad$LS$LS_enriched)
+# [1] "DFrame"
+# attr(,"package")
+# [1] "S4Vectors"
+names(markers.ls.t.1vAll.broad$LS$LS_enriched)
+# [1] "std.logFC"   "log.p.value" "log.FDR"     "non0median"
+class(markers.ls.t.pw.broad$LS)
+# [1] "DFrame"
+# attr(,"package")
+# [1] "S4Vectors"
+names(markers.ls.t.pw.broad$LS)
+names(markers.ls.t.pw.broad$LS$stats.Astro)
+# [1] "logFC"       "log.p.value" "log.FDR"
+
+
+#######################################################
+#### Reshape markers.ls.t.1vAll.broad (enriched) ####
+#######################################################
+
 markers.ls.t.1vAll.broad$LS$LS_enriched
 
+## Change column names
 colnames(markers.ls.t.1vAll.broad$LS$LS_enriched)[1:3] <- c("t_stat", "p_value", "fdr")
 markers.ls.t.1vAll.broad$LS$LS_enriched$ensembl <- rownames(markers.ls.t.1vAll.broad$LS$LS_enriched)
 
+## Add names from mgi data base
 mart <- useDataset("mmusculus_gene_ensembl", useMart("ensembl"))
 genes <- markers.ls.t.1vAll.broad$LS$LS_enriched$ensembl
 gene_list <- getBM(filters = "ensembl_gene_id", attributes = c("ensembl_gene_id", "mgi_symbol"), values = genes, mart = mart)
@@ -43,10 +60,14 @@ LS_enriched <- LS_enriched[, c(2:4, 1, 6)]
 colnames(LS_enriched)[5] <- "gene"
 
 
-modeling_results <- list(
-    "enrichment" = LS_enriched
-)
+#####################################################
+#### Reshape markers.ls.t.pw.broad$LS (pairwise) ####
+#####################################################
 
+markers.ls.t.pw.broad$LS
+markers.ls.t.pw.broad$LS$Astro
+
+## Creat list of name with comparations eg. _LS-Astro, _LS-
 stats_tiss <- stringr::str_match(
     string = names(markers.ls.t.pw.broad$LS),
     pattern = "stats\\..+"
@@ -56,17 +77,32 @@ stats_tiss <- stringr::str_match(
     str_remove("stats\\.")
 stats_tiss <- paste("_", "LS-", stats_tiss, sep = "")
 
-pairw<-as.data.frame(markers.ls.t.pw.broad$LS) %>% select(matches("stats\\..+"))
-new_names<-paste(c("t_stat", "p_value", "fdr"), rep(stats_tiss,each = 3), sep = "")
-names(pairw)<-new_names
-pairw$ensembl<-rownames(pairw)
+## Convert S4Vector object to DataFrame and selecting needed columns
+LS_pairw<-as.data.frame(markers.ls.t.pw.broad$LS) %>% select(matches("stats\\..+"))
 
-genes <- pairw$ensembl
+## Change column names
+new_names<-paste(c("t_stat", "p_value", "fdr"), rep(stats_tiss,each = 3), sep = "")
+names(LS_pairw)<-new_names
+LS_pairw$ensembl<-rownames(LS_pairw)
+
+## Add names from mgi data base
+genes <- LS_pairw$ensembl
 gene_list <- getBM(filters = "ensembl_gene_id", attributes = c("ensembl_gene_id", "mgi_symbol"), values = genes, mart = mart)
-pairw <- merge(pairw, gene_list, by.x = "ensembl", by.y = "ensembl_gene_id")
-l_names<-length(colnames(pairw))
-pairw[, c(2:(l_names-1),1,l_names)]
-pairw<-rename(pairw, gene = mgi_symbol)
+LS_pairw <- merge(LS_pairw, gene_list, by.x = "ensembl", by.y = "ensembl_gene_id")
+l_names<-length(colnames(LS_pairw))
+LS_pairw<-LS_pairw[, c(2:(l_names-1),1,l_names)]
+LS_pairw<-rename(LS_pairw, gene = mgi_symbol)
+
+
+########################################
+#### Create modeling_results object ####
+########################################
+
+modeling_results <- list(
+    "enrichment" = LS_enriched
+    "pairwise" = LS_pairw
+)
+
 
 
 ## Reproducibility information
