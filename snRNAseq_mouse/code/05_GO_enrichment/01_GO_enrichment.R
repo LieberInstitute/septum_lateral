4d66e8cc916e2bb131d20e5d6ab48cfc385c1886
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-03-06 16:59:02 -0600

    Rerun go analysis with clusterProfiler::compareCluster in case new info is available

--- snRNAseq_mouse/code/05_GO_enrichment/01_GO_enrichment.R
+++ snRNAseq_mouse/code/05_GO_enrichment/01_GO_enrichment.R
@@ -14,3 +14,8 @@ sigGene <- fread(file = "/dcl01/lieber/ajaffe/Keri/TrkBKO/tables/sigGenes_DE_FDR
 
 geneUniverse <- fread(file = "/dcl01/lieber/ajaffe/Keri/TrkBKO/tables/allGenes_DE_noCut.csv", header = TRUE, sep = ",", data.table = FALSE, stringsAsFactors = FALSE)
 
+go <- compareCluster(sigGene, fun = "enrichGO",
+                universe = geneUniverse, OrgDb = org.Mm.eg.db,
+                ont = "ALL", pAdjustMethod = "BH",
+                pvalueCutoff  = 0.1, qvalueCutoff  = 0.05,
+				readable= TRUE)
