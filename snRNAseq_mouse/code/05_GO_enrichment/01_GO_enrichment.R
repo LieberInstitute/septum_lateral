42a9c61d2532948b583118aeb1e74e5ba6b3385c
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-03-07 09:58:56 -0600

    Created function to plot GO for each dataset (CC, BP, MF). Added reproducibility information.

--- snRNAseq_mouse/code/05_GO_enrichment/01_GO_enrichment.R
+++ snRNAseq_mouse/code/05_GO_enrichment/01_GO_enrichment.R
@@ -14,8 +14,61 @@ sigGene <- fread(file = "/dcl01/lieber/ajaffe/Keri/TrkBKO/tables/sigGenes_DE_FDR
 
 geneUniverse <- fread(file = "/dcl01/lieber/ajaffe/Keri/TrkBKO/tables/allGenes_DE_noCut.csv", header = TRUE, sep = ",", data.table = FALSE, stringsAsFactors = FALSE)
 
-go <- compareCluster(sigGene, fun = "enrichGO",
-                universe = geneUniverse, OrgDb = org.Mm.eg.db,
-                ont = "ALL", pAdjustMethod = "BH",
-                pvalueCutoff  = 0.1, qvalueCutoff  = 0.05,
-				readable= TRUE)
+sigGene <- split(sigGene$EntrezID, sign(sigGene$logFC))
+sigGene <- lapply(sigGene, function(x) x[!is.na(x)])
+
+geneUniverse <- as.character(geneUniverse$EntrezID)
+geneUniverse <- geneUniverse[!is.na(geneUniverse)]
+
+
+####################################
+#### Run GO enrichment analysis ####
+####################################
+
+go <- compareCluster(sigGene,
+    fun = "enrichGO",
+    universe = geneUniverse, OrgDb = org.Mm.eg.db,
+    ont = "ALL", pAdjustMethod = "BH",
+    pvalueCutoff = 0.1, qvalueCutoff = 0.05,
+    readable = TRUE
+)
+
+
+############################
+#### Plot BP, CC and MF ####
+############################
+
+plot_go <- function(ont, title_p, path, filename) {
+    dotplot_1 <- ggplot(filter(go, ONTOLOGY == ont), aes(Cluster, Description)) +
+        theme_bw() +
+        geom_point(aes(color = p.adjust, size = Count)) +
+        scale_color_gradientn(
+            colours = c("#f7ca64", "#46bac2", "#7e62a3"),
+            trans = "log10",
+            guide = guide_colorbar(reverse = TRUE, order = 1)
+        ) +
+        scale_size_continuous(range = c(2, 10)) +
+        xlab("Cluster") +
+        ylab("") +
+        ggtitle(title_p)
+
+    ggsave(filename = filename, path = path, dotplot_1, height = 6, width = 7)
+}
+
+plot_go(ont = "BP", title_p = "Biological Process", filename = "GOenrichment_BP.pdf", path = here("snRNAseq_mouse", "plots/", "05_GO_enrichment/"))
+
+plot_go(ont = "CC", title_p = "Cellular Component", filename = "GOenrichment_CC.pdf", path = here("snRNAseq_mouse", "plots/", "05_GO_enrichment/"))
+
+plot_go(ont = "MF", title_p = "Molecular Function", filename = "GOenrichment_MF.pdf", path = here("snRNAseq_mouse", "plots/", "05_GO_enrichment/"))
+
+
+#####################################
+#### Reproducibility information ####
+#####################################
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
