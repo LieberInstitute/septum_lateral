a1286ef2929fbc8f7f9db5fedbad1fc1a7ddd446
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-04-17 08:31:01 -0600

    create function to extract gene names from significant genes

--- snRNAseq_mouse/code/04_clinical_set_enrichment/04_extract_gene_names.R
+++ snRNAseq_mouse/code/04_clinical_set_enrichment/04_extract_gene_names.R
@@ -1,6 +1,25 @@
 library("dplyr")
 library("here")
 library("sessioninfo")
+library("purrr")
+
+extract_sig_genes <- function(list_tiss, list_set, modeling_results) {
+    dims <- dim(modeling_results)[2]
+    lapply(list_tiss, function(list_tiss) {
+        list_tiss <- cbind(list_tiss, modeling_results[, (dims - 1):dims])
+        genes <- list_tiss %>%
+            filter(.[, 3] < 0.05) %>%
+            select(ensembl, gene)
+        if (list_set == "positive") {
+            inter_genes <- intersect(as.vector(genes$ensembl), gene_list_FDR01$positive)
+        } else {
+            inter_genes <- intersect(as.vector(genes$ensembl), gene_list_FDR01$negative)
+        }
+        genes <- genes %>% filter(ensembl %in% inter_genes)
+        return(genes)
+    })
+}
+
 
 
 ############### Load objects with inputs from gene_set_enrichment #############
@@ -15,5 +34,21 @@ load(
     verbose = TRUE
 )
 
+###############################################################################
 
+
+
+################### Extract gene names and create data frame ##################
+
+## Select genes by tissue type from broad data
+broad_list_sep <- split.default(modeling_results_broad$enrichment[, 1:54], rep(1:18, each = 3))
+
+broad_list_genes_neg <- extract_sig_genes(list_tiss = broad_list_sep, list_set = "negative", modeling_results = modeling_results_broad$enrichment)
+broad_list_genes_pos <- extract_sig_genes(list_tiss = broad_list_sep, list_set = "positive", modeling_results = modeling_results_broad$enrichment)
+
+## Select genes by tissue type from not-broad data
+list_sep <- split.default(modeling_results$enrichment[, 1:48], rep(1:16, each = 3))
+
+list_genes_neg <- extract_sig_genes(list_tiss = list_sep, list_set = "negative", modeling_results = modeling_results$enrichment)
+list_genes_pos <- extract_sig_genes(list_tiss = list_sep, list_set = "positive", modeling_results = modeling_results$enrichment)
 
