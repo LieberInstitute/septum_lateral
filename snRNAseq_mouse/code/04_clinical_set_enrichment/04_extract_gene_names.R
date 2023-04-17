c434949b81aa65f20c83075f6df56deabb3854ab
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-04-17 08:57:33 -0600

    add cell type names to the list

--- snRNAseq_mouse/code/04_clinical_set_enrichment/04_extract_gene_names.R
+++ snRNAseq_mouse/code/04_clinical_set_enrichment/04_extract_gene_names.R
@@ -38,17 +38,29 @@ load(
 
 
 
-################### Extract gene names and create data frame ##################
+######### Extract gene names from each data set (broad and not-broad) #########
 
 ## Select genes by tissue type from broad data
 broad_list_sep <- split.default(modeling_results_broad$enrichment[, 1:54], rep(1:18, each = 3))
+ct_names <- unique(gsub(names(modeling_results_broad$enrichment)[-(55:56)], pattern = ".+\\_", replacement = ""))
 
 broad_list_genes_neg <- extract_sig_genes(list_tiss = broad_list_sep, list_set = "negative", modeling_results = modeling_results_broad$enrichment)
+names(broad_list_genes_neg) <- ct_names
+
 broad_list_genes_pos <- extract_sig_genes(list_tiss = broad_list_sep, list_set = "positive", modeling_results = modeling_results_broad$enrichment)
+names(broad_list_genes_pos) <- ct_names
 
 ## Select genes by tissue type from not-broad data
 list_sep <- split.default(modeling_results$enrichment[, 1:48], rep(1:16, each = 3))
+ct_clus_names <- gsub(names(modeling_results$enrichment)[-(49:50)], pattern = "._[a-z]+_", replacement = "")
+ct_clus_names <- unique(gsub(ct_clus_names, pattern = "fdr\\_", replacement = ""))
 
 list_genes_neg <- extract_sig_genes(list_tiss = list_sep, list_set = "negative", modeling_results = modeling_results$enrichment)
+names(list_genes_neg) <- ct_clus_names
+
 list_genes_pos <- extract_sig_genes(list_tiss = list_sep, list_set = "positive", modeling_results = modeling_results$enrichment)
+names(list_genes_pos) <- ct_clus_names
+
+
+
 
