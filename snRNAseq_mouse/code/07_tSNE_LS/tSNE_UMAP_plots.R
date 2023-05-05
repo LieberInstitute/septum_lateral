5457bbf27e5ad3f1ae2ec6e7edc9bfecb2de621b
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-05 09:33:18 -0600

    create UMAP plots, same as tSNE

--- snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
+++ snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
@@ -59,3 +59,23 @@ tSNE_LS_no_legend <- ggcells(sce.ls.LS, mapping = aes(x = TSNE.1, y = TSNE.2, co
 tSNE_LS_no_legend +
     facet_wrap(~cellType.final)
 
+
+UMAP_cellTypes_no_legend <- ggcells(sce.ls.filter, mapping = aes(x = UMAP.1, y = UMAP.2, colour = cellType.final)) +
+    geom_point(size = 0.2, alpha = 0.3) +
+    scale_color_manual(values = cell_colors.ls) +
+    my_theme +
+    coord_equal() +
+    labs(x = "UMAP Dimension 1", y = "UMAP Dimension 2") +
+    theme(legend.position = "None")
+
+UMAP_LS_no_legend <- ggcells(sce.ls.LS, mapping = aes(x = UMAP.1, y = UMAP.2, colour = cellType.final)) +
+    geom_point(size = 0.2, alpha = 0.3) +
+    scale_color_manual(values = cell_colors.ls) +
+    my_theme +
+    coord_equal() +
+    labs(x = "UMAP Dimension 1", y = "UMAP Dimension 2") +
+    theme(legend.position = "None")
+
+UMAP_LS_no_legend +
+    facet_wrap(~cellType.final)
+
