130cdf405ab245e167bc286b8c65a404356e5947
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-05 09:32:41 -0600

    create tSNE plots. One with all the cell types and one that is favet wrap by LS clusters

--- snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
+++ snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
@@ -7,6 +7,13 @@ library("scater")
 library("CATALYST")
 library("sessioninfo")
 
+
+my_theme <- theme_bw() +
+    theme(
+        text = element_text(size = 15),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()
+    )
+
 load(
     here(
         "snRNAseq_mouse",
@@ -31,4 +38,24 @@ sce.ls.filter <- filterSCE(sce.ls, cellType.final %in% keepnames)
 sce.ls.LS <- filterSCE(sce.ls, cellType.final %in% LSnames)
 
 cell_colors.ls[names(cell_colors.ls)[-c(10:18)]] <- "#595E60"
+
+tSNE_cellTypes_no_legend <- ggcells(sce.ls.filter, mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType.final)) +
+    geom_point(size = 0.2, alpha = 0.3) +
+    scale_color_manual(values = cell_colors.ls) +
+    my_theme +
+    coord_equal() +
+    labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2") +
+    theme(legend.position = "None")
+
+
+tSNE_LS_no_legend <- ggcells(sce.ls.LS, mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType.final)) +
+    geom_point(size = 0.2, alpha = 0.3) +
+    scale_color_manual(values = cell_colors.ls) +
+    my_theme +
+    coord_equal() +
+    labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2") +
+    theme(legend.position = "None")
+
+tSNE_LS_no_legend +
+    facet_wrap(~cellType.final)
 
