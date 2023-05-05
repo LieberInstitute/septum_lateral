f3fbea08cd09b10a2b1fc15dad071722c6135eeb
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-05 09:41:58 -0600

    commented script and added session info

--- snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
+++ snRNAseq_mouse/code/07_tSNE_LS/tSNE_UMAP_plots.R
@@ -8,11 +8,8 @@ library("CATALYST")
 library("sessioninfo")
 
 
-my_theme <- theme_bw() +
-    theme(
-        text = element_text(size = 15),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank()
-    )
+
+############################### Load sce object ###############################
 
 load(
     here(
@@ -31,13 +28,40 @@ load(
 
 sce.ls
 
+###############################################################################
+
+
+
+############################## Filter sce object ##############################
+
+## sce.ls.filter has all cell types except the ones that needed to be dropped
+## sce.ls.LS has only LS clusters
+
 keepnames <- levels(colData(sce.ls)$cellType.final)[-c(4:6, 24)]
 LSnames <- levels(colData(sce.ls)$cellType.final)[10:18]
 
 sce.ls.filter <- filterSCE(sce.ls, cellType.final %in% keepnames)
 sce.ls.LS <- filterSCE(sce.ls, cellType.final %in% LSnames)
 
+###############################################################################
+
+
+
+############################### Plot aesthetics ###############################
+
+my_theme <- theme_bw() +
+    theme(
+        text = element_text(size = 15),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()
+    )
+
 cell_colors.ls[names(cell_colors.ls)[-c(10:18)]] <- "#595E60"
+
+###############################################################################
+
+
+
+################################# tSNE plots ##################################
 
 tSNE_cellTypes_no_legend <- ggcells(sce.ls.filter, mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType.final)) +
     geom_point(size = 0.2, alpha = 0.3) +
@@ -59,6 +83,11 @@ tSNE_LS_no_legend <- ggcells(sce.ls.LS, mapping = aes(x = TSNE.1, y = TSNE.2, co
 tSNE_LS_no_legend +
     facet_wrap(~cellType.final)
 
+###############################################################################
+
+
+
+################################# UMAP plots ##################################
 
 UMAP_cellTypes_no_legend <- ggcells(sce.ls.filter, mapping = aes(x = UMAP.1, y = UMAP.2, colour = cellType.final)) +
     geom_point(size = 0.2, alpha = 0.3) +
@@ -79,3 +108,17 @@ UMAP_LS_no_legend <- ggcells(sce.ls.LS, mapping = aes(x = UMAP.1, y = UMAP.2, co
 UMAP_LS_no_legend +
     facet_wrap(~cellType.final)
 
+###############################################################################
+
+
+
+######################### Reproducibility information #########################
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+###############################################################################
