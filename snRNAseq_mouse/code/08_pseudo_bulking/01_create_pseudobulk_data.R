92e04116bcbbc3d94d94955a448025f7fc09dab1
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-09 13:54:56 -0600

    pseudobulking and PC computation for LS clusters. Based on https://github.com/LieberInstitute/spatialDLPFC/blob/f700e3aef373e6d394787ff5f021f0c92e02ed1f/code/analysis/07_layer_differential_expression/01_create_pseudobulk_data.R#L52-L95 Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- snRNAseq_mouse/code/08_pseudo_bulking/01_create_pseudobulk_data.R
+++ snRNAseq_mouse/code/08_pseudo_bulking/01_create_pseudobulk_data.R
@@ -1,8 +1,11 @@
 library("here")
 library("spatialLIBD")
+library("SingleCellExperiment")
 library("scater")
+library("jaffelab")
 library("CATALYST")
 library("sessioninfo")
+
 
 ############################### Load sce object ###############################
 
@@ -40,3 +43,37 @@ sce.ls.LS <- filterSCE(sce.ls, cellType.final %in% LS_names)
 
 ###############################################################################
 
+
+
+############################# pseudobulking for LS ############################
+
+## pseudobulking across LS clusters
+sce_pseudo_LS <-
+    registration_pseudobulk(sce.ls.LS,
+        var_registration = "cellType.final",
+        var_sample_id = "Sample",
+        min_ncells = 10
+    )
+
+dim(sce_pseudo_LS)
+colData(sce_pseudo_LS)
+
+## Compute PCs
+pca <- prcomp(t(assays(sce_pseudo_LS)$logcounts))
+#metadata(sce_pseudo_LS) <- list("PCA_var_explained" = jaffelab::getPcaVars(pca)[seq_len(20)])
+#metadata(sce_pseudo_LS)
+pca_pseudo <- pca$x[, seq_len(20)]
+colnames(pca_pseudo) <- paste0("PC", sprintf("%02d", seq_len(ncol(pca_pseudo))))
+reducedDims(sce_pseudo_LS) <- list(PCA = pca_pseudo)
+
+## Compute some reduced dims
+set.seed(20230509)
+sce_pseudo_LS <- scater::runMDS(sce_pseudo_LS, ncomponents = 20)
+sce_pseudo_LS <- scater::runPCA(sce_pseudo_LS, name = "runPCA")
+# Warning in (function (A, nv = 5, nu = nv, maxit = 1000, work = nv + 7, reorth = TRUE,  :
+#   You're computing too large a percentage of total singular values, use a standard svd instead.
+# Warning message:
+# In check_numbers(k = k, nu = nu, nv = nv, limit = min(dim(x)) -  :
+#   more singular values/vectors requested than available
+
+###############################################################################
