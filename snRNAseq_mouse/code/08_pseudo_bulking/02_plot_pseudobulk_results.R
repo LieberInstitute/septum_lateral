d033b2e8c800f8e81145dbf37027580ce2d5d99e
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-14 10:22:36 -0600

    plotting pseudobulking (PCAs). This is based in https://github.com/LieberInstitute/spatialDLPFC/blob/f700e3aef373e6d394787ff5f021f0c92e02ed1f/code/analysis/07_layer_differential_expression/02_explore_expr_variability.R#L64C1-L81
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- snRNAseq_mouse/code/08_pseudo_bulking/02_plot_pseudobulk_results.R
+++ snRNAseq_mouse/code/08_pseudo_bulking/02_plot_pseudobulk_results.R
@@ -44,3 +44,40 @@ cell_colors_LS[1:9] <- c("#58B4E4","#169F74", "#0673B4", "#D56128", "#CC79A8", "
 
 ###############################################################################
 
+
+
+############################ Plot LS and broad PCs ############################
+
+## LS clusters
+p_LS <- plotPCA(
+    sce_pseudo_LS,
+    colour_by = "cellType.final",
+    ncomponents = 4,
+    point_size = 2,
+    label_format = c("%s %02i", " (%i%%)")
+    #percentVar = metadata(sce_pseudo)$PCA_var_explained
+    ) + scale_color_manual("cellType.final",values = cell_colors_LS)
+
+pdf(file = here("snRNAseq_mouse/plots/sce_pseudo_LS_PCs.pdf"), width = 8, height = 8)
+print(p_LS)
+dev.off()
+
+## Broad clusters
+p_all <- plotPCA(
+    sce_pseudo_all,
+    colour_by = "cellType.broad",
+    ncomponents = 12,
+    point_size = 1,
+    label_format = c("%s %02i", " (%i%%)"),
+    #percentVar = metadata(sce_pseudo)$PCA_var_explained
+    )
+
+# This needs to be change
+#p_all <- p_all + scale_color_manual("cellType.final", values = cell_colors.ls)
+
+pdf(file = here("snRNAseq_mouse/plots/sce_pseudo_broad_PCs.pdf"), width = 14, height = 14)
+print(p_all)
+dev.off()
+
+###############################################################################
+
