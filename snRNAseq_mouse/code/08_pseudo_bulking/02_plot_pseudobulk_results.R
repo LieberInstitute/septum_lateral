f06ad63881230fd0ce33b711225065528ef3ea8b
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-14 10:19:48 -0600

    Started script to plot pseudobulking for LS and broad clusters.

---
+++ snRNAseq_mouse/code/08_pseudo_bulking/02_plot_pseudobulk_results.R
@@ -0,0 +1,37 @@
+library("here")
+library("SingleCellExperiment")
+library("scater")
+library("sessioninfo")
+
+################### Load sce object and pseudobulk results ####################
+
+load(
+    here(
+        "snRNAseq_mouse",
+        "processed_data",
+        "SCE",
+        "sce_updated_LS.rda"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   sce.ls
+#   annotationTab.ls
+#   cell_colors.ls
+
+load(
+    here(
+        "snRNAseq_mouse",
+        "processed_data",
+        "SCE",
+        "sce_pseudobulking_LS_and_broad.rda"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   sce_pseudo_LS
+#   sce_pseudo_all
+
+###############################################################################
+
+
