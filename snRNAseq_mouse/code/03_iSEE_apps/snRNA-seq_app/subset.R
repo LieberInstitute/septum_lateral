38e79f3bce73bd99d797e355e7ad91f95eca6d55
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-22 10:46:29 -0400

    Auto-styled code

--- snRNAseq_mouse/code/03_iSEE_apps/snRNA-seq_app/subset.R
+++ snRNAseq_mouse/code/03_iSEE_apps/snRNA-seq_app/subset.R
@@ -1,5 +1,13 @@
 ### For iSEE shiny app =============
-load(here("snRNAseq_mouse", "processed_data", "SCE", "sce_updated_LS.rda"), verbose = T)
+load(
+    here(
+        "snRNAseq_mouse",
+        "processed_data",
+        "SCE",
+        "sce_updated_LS.rda"
+    ),
+    verbose = TRUE
+)
 
 # As-is size:
 lobstr::obj_size(sce.ls) / 1024^3 # 7.17 GB
@@ -10,8 +18,10 @@ sce.ls <- sce.ls[, -grep("Neuron.mixed", sce.ls$cellType.final)]
 sce.ls$cellType.final <- droplevels(sce.ls$cellType.final)
 
 # Corresponding hexadecimal colors
-cell_colors.ls <- cell_colors.ls[-grep("drop", names(cell_colors.ls))]
-cell_colors.ls <- cell_colors.ls[-grep("Neuron.mixed", names(cell_colors.ls))]
+cell_colors.ls <-
+    cell_colors.ls[-grep("drop", names(cell_colors.ls))]
+cell_colors.ls <-
+    cell_colors.ls[-grep("Neuron.mixed", names(cell_colors.ls))]
 
 sce.hold <- sce.ls
 
@@ -25,7 +35,8 @@ reducedDim(sce.ls, "GLMPCA_approx") <- NULL
 lobstr::obj_size(sce.ls) / 1024^3 # 0.80 GB
 
 # Because rownames are currently in Ensembl ID, re-assign & 'uniquify' to gene symbols
-rownames(sce.ls) <- uniquifyFeatureNames(rowData(sce.ls)$gene_id, rowData(sce.ls)$gene_name)
+rownames(sce.ls) <-
+    uniquifyFeatureNames(rowData(sce.ls)$gene_id, rowData(sce.ls)$gene_name)
 
 lobstr::obj_size(sce.ls) / 1024^3
 
@@ -57,7 +68,13 @@ rowData(sce.ls) <- rowdat.sce
 ## Clean up unnecessary info
 keepCols <- setdiff(
     colnames(colData(sce.ls)),
-    c("high.mito", "sizeFactor", "clusters.glmpca", "cellType", "cellType.exp")
+    c(
+        "high.mito",
+        "sizeFactor",
+        "clusters.glmpca",
+        "cellType",
+        "cellType.exp"
+    )
 )
 colData(sce.ls) <- colData(sce.ls)[, keepCols]
 
@@ -68,6 +85,13 @@ sce.ls.small <- sce.ls
 cell_cols.clean <- cell_colors.ls
 
 ## Save this
-save(sce.ls.small, cell_cols.clean,
-    file = here("snRNAseq_mouse", "processed_data", "SCE", "sce_for_iSEE_LS.rda")
+save(
+    sce.ls.small,
+    cell_cols.clean,
+    file = here(
+        "snRNAseq_mouse",
+        "processed_data",
+        "SCE",
+        "sce_for_iSEE_LS.rda"
+    )
 )
